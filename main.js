f932873d1ede7eda343536aee8586c95c9d60f27
Hannah.Celemen <hcelemen@icloud.com>
2022-08-05 09:39:25 -0600

    Add board functionality

--- main.js
+++ main.js
@@ -1,19 +1,57 @@
 //query selectors here
-
+const boardHeader = document.querySelector('.board__header');
+const boardGame = document.querySelector('.board__game');
+const gameBoxes = document.querySelectorAll('.board__game--box');
+// console.log(gameBoxes);
+const player1Token = document.querySelector('.player__1');
+const player1Score = document.querySelector('.player1__score');
+const player2Token = document.querySelector('.player__2');
+const player2Score = document.querySelector('.player2__score');
 
 
 //global variables here
-const playGame = new Game('X', 'O');
+const currentGame = new Game('Bubble Tea', 'Take Out Coffee');
+const player1 = currentGame.player1;
+const player2 = currentGame.player2;
 
 //event listeners here
-window.addEventListener('load', createPlayer);
+window.addEventListener('load', setUpBoard);
 
-
-//functions here
-function createPlayer(token) {
-  return new Player(token);
+for (let i = 0; i < gameBoxes.length; i++) {
+  const gameBox = gameBoxes[i];
+  
+  gameBox.addEventListener('click', function() {
+    currentGame.addToken(i);
+    console.log(currentGame);
+    currentGame.switchPlayer();
+    showPlayerTurn();
+  });
 }
 
 
-console.log(playGame)
+//functions here
+function setUpBoard() {
+  currentGame.createBoard();
+  showPlayerTurn();
+}
+
+function showPlayerTurn() {
+  let playerToken =  '';
+  console.log(currentGame.playerTurn, '-', player1.token)
+  if (currentGame.playerTurn === player1.token) {
+    playerToken = `<img class="player__1 player__token--smaller" src="./assets/icons8-bubble-tea-80.png" alt="Bubble tea token">`;
+  } else {
+    playerToken = `<img class="player__2 player__token--smaller" src="./assets/icons8-coffee-to-go-80.png" alt="Coffee token">`;
+  }
+  boardHeader.innerHTML = `<h1 class="board__title">It is ${playerToken}'s turn!</h1>`;
+}
+
+function renderBoard() {
+  for (let i = 0; i < gameBoxes.length; i++) {
+    
+  }
+}
+
+
+console.log(currentGame, player1, player2)
 
