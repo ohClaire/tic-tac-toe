81de5ea9d10c3ee34aa9e5bba5672bc5fff3f78b
Hannah.Celemen <hcelemen@icloud.com>
2022-08-06 22:24:34 -0600

    Refactor methods

--- game.js
+++ game.js
@@ -1,25 +1,10 @@
 class Game {
   constructor(playerToken1, playerToken2) {
+    this.board = [];
     this.player1 = new Player(playerToken1);
     this.player2 = new Player(playerToken2);
-    this.board = [];
-    this.playerTurn = this.player1;
+    this.playerTurn = this.player1.token;
     this.winner = '';
-    // console.log(this.createBoard())
-    // console.log(this.addToken(0), this.switchPlayer());
-    // console.log(this.addToken(1));
-    // console.log(this.addToken(2));
-    // console.log(this.addToken(3));
-    // console.log(this.addToken(4));
-    // console.log(this.addToken(5));
-    // console.log(this.addToken(6));
-    // console.log(this.addToken(7));
-    // console.log(this.addToken(8));
-    // console.log(this.checkForWinner());
-    // console.log(this.checkForDraw());
-    // console.log(this.declareWinner(), this.winner)
-    // console.log(this.declareMatch())
-    // console.log(this.board)
   }
 
   createBoard() {
@@ -30,57 +15,54 @@ class Game {
     }
   }
 
-  addToken(index) {
+  addTokenToBoard(index) {
     if (this.board[index] === '') {
-      this.board[index] = this.playerTurn.token;
-    } else {
-      return 'Choose a different spot!';
-    }
-    // return this.switchPlayer(player);
+      this.board[index] = this.playerTurn;
+      
+      this.switchPlayerTurn(); 
+    } 
   }
 
-  switchPlayer() {
+  switchPlayerTurn() {
     if (this.playerTurn === this.player1.token) {
       this.playerTurn = this.player2.token;
     } else {
       this.playerTurn = this.player1.token;
     } 
-
-    // return `It is ${this.playerTurn}'s turn!`
   } 
+
+  updateWinner(index) {
+    this.winner = this.board[index];
+  }
   
   checkForWinner() {
-    let isThereWinner = false; //after the whole board is checked, return if a winner is found
-    // console.log('check winner', this.board[0])
+
     if (this.detectThreeMatches(0, 1, 2)) {
-      this.winner = this.board[0];
-      isThereWinner = true;
+      this.updateWinner(0);
+
     } else if (this.detectThreeMatches(3, 4, 5)) {
-      this.winner = this.board[3];
-      isThereWinner = true;
+      this.updateWinner(3);
+
     } else if (this.detectThreeMatches(6, 7, 8)) {
-      this.winner = this.board[6];
-      isThereWinner = true;
+      this.updateWinner(6);
+
     } else if (this.detectThreeMatches(0, 3, 6)) {
-      this.winner = this.board[0];
-      isThereWinner = true;
+      this.updateWinner(0);
+
     } else if (this.detectThreeMatches(1, 4, 7)) {
-      this.winner = this.board[1];
-      isThereWinner = true;
+      this.updateWinner(1);
+
     } else if (this.detectThreeMatches(2, 5, 8)) {
-      this.winner = this.board[2];
-      isThereWinner = true;
+      this.updateWinner(2);
+     
     } else if (this.detectThreeMatches(0, 4, 8)) {
-      this.winner = this.board[0];
-      isThereWinner = true;
+      this.updateWinner(0);
+     
     } else if (this.detectThreeMatches(2, 4, 6)) {
-      this.winner = this.board[2];
-      isThereWinner = true;
+      this.updateWinner(2);
+    
     }
-    // this.declareWinner(this.winner);
-    // createBoard()
-    console.log(isThereWinner);
-    return isThereWinner;
+
   }
 
   detectThreeMatches(boxIndex1, boxIndex2, boxIndex3) { 
@@ -88,34 +70,10 @@ class Game {
       this.board[boxIndex1] === this.board[boxIndex2] && 
       this.board[boxIndex1] === this.board[boxIndex3]
     ) {
-      // console.log('this is the winner!')
+
       return true;
     }
   }
-  
-  getPlayer(playerToken) {
-    if (playerToken === 'X') {
-      return this.player1;
-    } else {
-      return this.player2;
-    }
-  }
 
-  givePlayerWin(playerToken) {
-    const player = this.getPlayer(playerToken);
-
-    player.wins += 1; 
-  }
-
-
-  declareMatch() {
-    const isThereWinner = this.checkForWinner();
-
-    if (!isThereWinner && !this.board.includes('')) { //if there is NOT a winner and there are no empty strings in the board
-      return 'You\'re evenly matched!'
-    } 
-    
-    return `${this.winner} has won this game!`; 
-  }
 
 }
