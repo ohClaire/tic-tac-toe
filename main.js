cee4a0d171b57fd80133ac8ccc283c7a1bb3a343
Hannah.Celemen <hcelemen@icloud.com>
2022-08-07 20:48:09 -0600

    Refactor to clearly separate updating data model and updating DOM

--- main.js
+++ main.js
@@ -9,25 +9,26 @@ const player2Score = document.querySelector('.player2__score');
 
 //global variables here
 const currentGame = new Game('Bubble Tea', 'Take Out Coffee');
-const player1 = currentGame.player1;
-const player2 = currentGame.player2;
 const bubbleTeaToken = `<img class="player__1 player__token--smaller" src="./assets/icons8-bubble-tea-80.png" alt="Bubble tea token">`;
 const coffeeCupToken = `<img class="player__2 player__token--smaller" src="./assets/icons8-coffee-to-go-80.png" alt="Coffee token">`;
+const player1 = currentGame.player1;
+const player2 = currentGame.player2;
 
 //event listeners here
 window.addEventListener('load', setUpBoard);
+
 
 for (let i = 0; i < gameBoxes.length; i++) {
   const gameBox = gameBoxes[i];
   
   gameBox.addEventListener('click', function() {
-    currentGame.addTokenToBoard(i);
-    showTokensOnBoard(gameBox, i);
-    currentGame.checkForWinner(); 
-    declarePlayerTurn();
-    declareMatch();
-    changeScore();
+    if (currentGame.winner === '') {
+      currentGame.addTokenToBoard(index);
 
+      renderTokenOnBoard(gameBox, i);
+      renderCenterBanner();
+      renderScore();
+    }
   });
 }
 // setTimeout(createBoard, 5000);
@@ -36,22 +37,18 @@ for (let i = 0; i < gameBoxes.length; i++) {
 function setUpBoard() {
   currentGame.createBoard();
   
-  declarePlayerTurn();
+  renderCenterBanner();
 
-  clearScores(player1);
+  // clearScores(player1);
 
-  clearScores(player2);
+  // clearScores(player2);
 }
 
-function declarePlayerTurn() {
-  let playerToken =  renderPlayerToken();
-  
-  boardBanner.innerHTML = `<h1 class="board__title">It is ${playerToken}'s turn!</h1>`;
-}
 
-function renderPlayerToken() {  
 
-  if (currentGame.playerTurn === player1.token) {
+function getTokenImage(playerToken) {  
+
+  if (playerToken === player1.token) {
     return bubbleTeaToken;
 
   } else {
@@ -59,8 +56,8 @@ function renderPlayerToken() {
   }
 }
 
-function showTokensOnBoard(box, index) {
-  
+function renderTokenOnBoard(box, index) {
+
   if (currentGame.board[index] === 'Bubble Tea') {
     box.innerHTML = bubbleTeaToken;
 
@@ -69,43 +66,29 @@ function showTokensOnBoard(box, index) {
   }
 }
 
-function getTokenImg(winner) {
-  if (winner === 'Bubble Tea') {
-    return bubbleTeaToken;
+function renderCenterBanner() {
 
-  } else {
-    return coffeeCupToken
-  }
-}
-
-function declareMatch() {
   if (currentGame.winner) {
-    boardBanner.innerHTML = `<h1 class="board__title">${getTokenImg(currentGame.winner)} wins!</h1>`;
+    boardBanner.innerHTML = `<h1 class="board__title">${getTokenImage(currentGame.winner)} wins!</h1>`;
 
   } else if (!currentGame.board.includes('')) {
     boardBanner.innerHTML = `<h1 class="board__title">It's a draw!</h1>`;
-  }
 
+  } else {
+    boardBanner.innerHTML = `<h1 class="board__title">It is ${getTokenImage(currentGame.playerTurn)}'s turn!</h1>`;
+  }
 }
 
+function renderScore() {
+  player1Score.innerText = `${player1.wins} wins`;
 
-function changeScore() {
-  
-  if (currentGame.winner === player1.token) {
-    player1.wins += 1;
-    
-    player1Score.innerText = `${player1.wins} wins`;
-  } 
+  player2Score.innerText = `${player2.wins} wins`;
 
-  if (currentGame.winner === player2.token) {
-    player2.wins += 1;
-
-    player2Score.innerText = `${player2.wins} wins`;
-  }
 }
 
 function clearScores(player) {
   player.wins = 0;
 }
+
 
 
