d2d708db5dbe7d5ce25b29995e01c598c108c6d6
Hannah.Celemen <hcelemen@icloud.com>
2022-08-08 20:01:24 -0600

    Refactor and reset game after winner or draw

--- main.js
+++ main.js
@@ -1,73 +1,49 @@
 //query selectors here
 const boardBanner = document.querySelector('.board__banner');
-const boardGame = document.querySelector('.board__game');
 const gameBoxes = document.querySelectorAll('.board__game--box');
-const player1Token = document.querySelector('.player__1');
 const player1Score = document.querySelector('.player1__score');
-const player2Token = document.querySelector('.player__2');
 const player2Score = document.querySelector('.player2__score');
 
 //global variables here
-const currentGame = new Game('Bubble Tea', 'Take Out Coffee');
 const bubbleTeaToken = `<img class="player__1 player__token--smaller" src="./assets/icons8-bubble-tea-80.png" alt="Bubble tea token">`;
 const coffeeCupToken = `<img class="player__2 player__token--smaller" src="./assets/icons8-coffee-to-go-80.png" alt="Coffee token">`;
+const currentGame = new Game('Bubble Tea', 'Take Out Coffee');
 const player1 = currentGame.player1;
 const player2 = currentGame.player2;
 
 //event listeners here
-window.addEventListener('load', setUpBoard);
+window.addEventListener('load', renderCenterBanner);
 
 
 for (let i = 0; i < gameBoxes.length; i++) {
   const gameBox = gameBoxes[i];
   
   gameBox.addEventListener('click', function() {
-    if (currentGame.winner === '') {
-      currentGame.addTokenToBoard(index);
+    if (currentGame.winner === '' && currentGame.board.includes('')) { 
+      currentGame.addTokenToBoard(i); 
+      renderChanges();
 
-      renderTokenOnBoard(gameBox, i);
-      renderCenterBanner();
-      renderScore();
-    }
+      if (currentGame.winner || !currentGame.board.includes('')) {
+        setTimeout(function() {
+          currentGame.resetGame();
+          renderChanges();
+        }, 3000);
+      }    
+    }  
   });
 }
-// setTimeout(createBoard, 5000);
-
-//functions here
-function setUpBoard() {
-  currentGame.createBoard();
-  
-  renderCenterBanner();
-
-  // clearScores(player1);
-
-  // clearScores(player2);
-}
-
-
 
 function getTokenImage(playerToken) {  
-
   if (playerToken === player1.token) {
     return bubbleTeaToken;
-
-  } else {
+  } else if (playerToken === player2.token) {
     return coffeeCupToken;
-  }
-}
-
-function renderTokenOnBoard(box, index) {
-
-  if (currentGame.board[index] === 'Bubble Tea') {
-    box.innerHTML = bubbleTeaToken;
-
   } else {
-    box.innerHTML = coffeeCupToken;
+    return '';
   }
 }
 
 function renderCenterBanner() {
-
   if (currentGame.winner) {
     boardBanner.innerHTML = `<h1 class="board__title">${getTokenImage(currentGame.winner)} wins!</h1>`;
 
@@ -81,14 +57,21 @@ function renderCenterBanner() {
 
 function renderScore() {
   player1Score.innerText = `${player1.wins} wins`;
-
   player2Score.innerText = `${player2.wins} wins`;
-
 }
 
-function clearScores(player) {
-  player.wins = 0;
+function renderGame() {
+  for (let i = 0; i < gameBoxes.length; i++) {
+    gameBoxes[i].innerHTML = getTokenImage(currentGame.board[i]);
+  }
 }
+
+function renderChanges() {
+  renderCenterBanner();
+  renderGame();
+  renderScore();
+}
+
 
 
 
